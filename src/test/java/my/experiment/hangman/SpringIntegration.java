326f731362871a9d84292c1aac969056762e8bfa
isil irem tekes <isil.irem.tekes@huawei.com>
2017-12-08 15:51:47 +0200

    add Docker to the project and fixed runnable method bug

--- src/test/java/my/experiment/hangman/SpringIntegration.java
+++ src/test/java/my/experiment/hangman/SpringIntegration.java
@@ -1,16 +1,13 @@
 package my.experiment.hangman;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Created by i00344757 on 08/12/2017.
  */
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 @SpringBootTest(classes = HangmanApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class SpringIntegration {
